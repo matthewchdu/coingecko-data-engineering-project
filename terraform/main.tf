996936232e284fe441459decc61e6552956fa107
Matthew <matthew.ch.du@gmail.com>
2026-08-20 17:48:50 +0100

    Implemented github scheduled daily ingest

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,3 +32,22 @@ resource "google_bigquery_dataset" "raw_dataset" {
   dataset_id = var.bq_dataset_name
   location   = var.region
 }
+
+resource "google_bigquery_table" "external_crypto_markets" {
+  dataset_id = google_bigquery_dataset.raw_dataset.dataset_id
+  table_id   = "raw_crypto_markets"
+
+  external_data_configuration {
+    autodetect    = true
+    source_format = "PARQUET"
+
+    source_uris = [
+      "gs://${google_storage_bucket.crypto_bucket.name}/raw/crypto_markets/*"
+    ]
+
+    hive_partitioning_options {
+      mode              = "AUTO"
+      source_uri_prefix = "gs://${google_storage_bucket.crypto_bucket.name}/raw/crypto_markets/"
+    }
+  }
+}
