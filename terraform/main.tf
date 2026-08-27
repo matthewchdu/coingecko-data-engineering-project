bbe2404e4e611ab5bad7c9613be4e5ae1b498245
Matthew <matthew.ch.du@gmail.com>
2026-08-27 21:28:59 +0100

    finished

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,3 +51,25 @@ resource "google_bigquery_table" "external_crypto_markets" {
     }
   }
 }
+
+resource "google_bigquery_table" "clean_crypto_markets_view" {
+  dataset_id          = google_bigquery_dataset.raw_dataset.dataset_id
+  table_id            = "clean_crypto_markets"
+  deletion_protection = false
+
+  view {
+    query          = <<EOF
+SELECT *
+FROM `${var.projName}.${google_bigquery_dataset.raw_dataset.dataset_id}.${google_bigquery_table.external_crypto_markets.table_id}`
+QUALIFY ROW_NUMBER() OVER (
+  PARTITION BY id, snapshot_date 
+  ORDER BY extracted_at DESC
+) = 1
+EOF
+    use_legacy_sql = false
+  }
+
+  depends_on = [
+    google_bigquery_table.external_crypto_markets
+  ]
+}
